f051cefb607539452a7fa489e012abe60c2089d6
BlankHue <42626093+BlankHue@users.noreply.github.com>
2018-11-19 01:31:20 -0800

    Update cs211_proj3.sh

--- cs211_proj3.sh
+++ cs211_proj3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=proj3
 #SBATCH --output=res.txt
-#SBATCH -N 8
+#SBATCH -N 1
 #SBATCH -t 00:59:00
 
-mpirun -np 256 ./cs211_proj3_1 100000
+mpirun -np 32 ./cs211_proj3_1 100000
