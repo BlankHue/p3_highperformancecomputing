e1087563694571acd3042f13afbe04d77ad7c909
BlankHue <42626093+BlankHue@users.noreply.github.com>
2018-11-19 03:05:15 -0800

    Update cs211_proj3_1.cpp

--- cs211_proj3_1.cpp
+++ cs211_proj3_1.cpp
@@ -1,36 +1,20 @@
 #include "mpi.h"
-
 #include <cmath>
-
 #include <cstdio>
-
 #include <cstring>
-
 #include <cstdint>
-
 #include <cstdlib>
-
 #define MIN(a,b)  ((a)<(b)?(a):(b))
 
-
-
 #define BLOCK_LOW(id,p,n)  ((id)*(n)/(p))
-
 #define BLOCK_HIGH(id,p,n) \
-
         ( BLOCK_LOW((id)+1,p,n)-1 ) 
 
 #define BLOCK_SIZE(id,p,n) \
-
         (BLOCK_LOW( (id)+1, p, n) -  BLOCK_LOW((id), p, n))
 
-
-
 #define BLOCK_OWNER(index,p,n) \
-
         ( ( ((p)*(index)+1)-1 ) / (n) )
-
-
 
 
 
