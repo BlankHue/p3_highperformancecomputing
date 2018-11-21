e09c4da879e085950de6d9d0f772c00fcd915579
BlankHue <42626093+BlankHue@users.noreply.github.com>
2018-11-20 20:30:36 -0800

    Update cs211_proj3_1.cpp

--- cs211_proj3_1.cpp
+++ cs211_proj3_1.cpp
@@ -104,10 +104,7 @@ int main(int argc, char*argv[])
         }
         for (i = first/2; i < size; i += prime/2) //added first - 2 (orig. i = first)
         {  
-              if((i*2+1) % prime == 0)
-            {
                 marked[i] = 1;
-            }
         }
         if (!id)
         {
