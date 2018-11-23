20f706bf365a99b8d1a677326b86f3d689832bd3
BlankHue <42626093+BlankHue@users.noreply.github.com>
2018-11-23 15:58:04 -0800

    Update cs211_proj3_1.cpp

--- cs211_proj3_1.cpp
+++ cs211_proj3_1.cpp
@@ -102,7 +102,7 @@ int main(int argc, char*argv[])
                 first = prime - ((low_value) % prime);
             }
         }
-        for (i = first; i < size*2; i += prime) //added first - 2 (orig. i = first)
+        for (i = first; i < size; i += prime) //added first - 2 (orig. i = first)
         {  
                 marked[i] = 1;
         }
