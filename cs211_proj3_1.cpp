f75e459caa8e771933b43d5077c570a0ed5d9d01
BlankHue <42626093+BlankHue@users.noreply.github.com>
2018-11-22 18:20:02 -0800

    Update cs211_proj3_1.cpp

--- cs211_proj3_1.cpp
+++ cs211_proj3_1.cpp
@@ -104,8 +104,8 @@ int main(int argc, char*argv[])
         }
         for (i = first/2; i < size; i += prime/2) //added first - 2 (orig. i = first)
         {  
-             if ((low_value + (i*2+1)) % prime == 0 )
-             {
+            // if ((low_value + (i*2+1)) % prime == 0 )
+           //  {
                 marked[(i/2)] = 1;
               //  cout << "Marking: " << i << endl;
           //  }
