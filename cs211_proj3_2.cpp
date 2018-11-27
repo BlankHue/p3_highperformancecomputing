b140512d0efbae2d3941bf302f7b83c87e514486
BlankHue <42626093+BlankHue@users.noreply.github.com>
2018-11-26 16:34:00 -0800

    Update cs211_proj3_2.cpp

--- cs211_proj3_2.cpp
+++ cs211_proj3_2.cpp
@@ -60,6 +60,7 @@ int main(int argc, char*argv[])
    // cout << "process id: " << id << endl;
    // cout << "size: " << size << endl;
     proc0_size = ((n*2)-1)/p;
+    cout << "process 0 size is: " << proc0_size << endl;
         
     if((2 + proc0_size) < (int) sqrt((double) n))
     {
