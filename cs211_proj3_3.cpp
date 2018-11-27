f915aa5a03d408a14c3fca4bf1065296c683a53d
BlankHue <42626093+BlankHue@users.noreply.github.com>
2018-11-26 22:21:33 -0800

    Update cs211_proj3_3.cpp

--- cs211_proj3_3.cpp
+++ cs211_proj3_3.cpp
@@ -173,7 +173,7 @@ int main(int argc, char*argv[])
             }
         }
            
-        for (i = (first/2) + ((low_value_block - low_value)/2); i < block_size; i += prime) //added first - 2 (orig. i = first)
+        for (i = (first/2) + ((low_value_block - low_value)/2); i < ((high_value_block-low_value_block)/2); i += prime) //added first - 2 (orig. i = first)
         {  
             if((low_value_block + (i*2)) % prime == 0)
             {
@@ -185,10 +185,10 @@ int main(int argc, char*argv[])
                 {
                         i+=1;
                 }
-                if(i < block_size)
-                {
+              //  if(i < block_size)
+               // {
                    marked2[i] = 1;
-                }
+                //}
             }
         }   
             while (marked[++index]);
